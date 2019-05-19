b66542033953ba9637e2b8b5a9f69a71a86950e9
Adam Sojka <adam.sojka@smadas.net>
2019-05-19 12:01:26 +0200

    Create PathSegmentation class
    Create PathSegmentation class

---
+++ include/path_segmentation/PathSegmentation.h
@@ -0,0 +1,14 @@
+//
+// Created by smadas on 19.5.2019.
+//
+
+#ifndef SRC_PATHSEGMENTATION_H
+#define SRC_PATHSEGMENTATION_H
+
+
+class PathSegmentation {
+
+};
+
+
+#endif //SRC_PATHSEGMENTATION_H
---
+++ src/PathSegmentation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smadas on 19.5.2019.
+//
+
+#include "../include/path_segmentation/PathSegmentation.h"
